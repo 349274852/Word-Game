be7a5d708b7cf7ffd1657f87b7be531c70423364
StevenLin7777 <49196491+StevenLin7777@users.noreply.github.com>
2019-04-11 08:50:13 -0400

    Add files via upload

---
+++ NewMainMenu/TitleText.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleText here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleText extends Actor
+{
+    /**
+     * Act - do whatever the TitleText wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }  
+    public TitleText()
+    {
+        getImage().scale(getImage().getWidth() / 3 + 70  , getImage().getHeight() / 2 + 20  );
+    }
+}
